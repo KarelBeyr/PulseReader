d6952cd9ef594aafb5f5c677c439874e3370b0d3
Karel Beyr <karel.beyr@cancom.com>
2025-08-01 21:36:27 +0200

    nice PWM graphics

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -23,6 +23,8 @@
 /* USER CODE BEGIN Includes */
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,7 +58,8 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void RenderPwmUart(uint16_t duty, uint32_t freq);
+static void UartSetCursorPosition(int row, int col);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -65,9 +68,9 @@ static void MX_TIM2_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -100,33 +103,48 @@ int main(void)
   HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
   // Start Input Capture for CH2 (falling edge)
   HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_2);
-  char msg[100];
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setbuf(stdout, NULL);
-  printf("Hello world!");
+  setbuf(stdout, NULL); // so that we do not need to add \r\n at the end of printf statements
+  int lastDuty = -10;
+  bool signalAcquired = false;
+  uint32_t lastPulse = 0;
   while (1)
   {
-	    if (new_data_available) {
-	        uint32_t period = rising - last_rising;
-	        uint32_t high_time = falling - last_rising;
+    uint32_t now = HAL_GetTick();
+    if (new_data_available)
+    {
+      new_data_available = 0;
+      lastPulse = now;
+      signalAcquired = true;
+      uint32_t period = rising - last_rising;
+      uint32_t high_time = falling - last_rising;
+      uint32_t freq = 0;
+      uint16_t duty = 0;
 
-	        uint32_t freq = 0;
-	        uint16_t duty = 0;
-
-	        if (period > 0) {
-	            freq = HAL_RCC_GetPCLK1Freq() * 2 / period;
-	            duty = (high_time * 100) / period;
-	            if (duty > 100) duty = duty - 100;
-	        }
-
-	        snprintf(msg, sizeof(msg), "Freq: %lu Hz, Duty: %d pct\r\n", freq, duty);
-	        HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-
-	        new_data_available = 0;
-	    }
+      if (period > 0)
+      {
+        freq = HAL_RCC_GetPCLK1Freq() * 2 / period;
+        duty = (high_time * 100) / period;
+        if (duty > 100)
+          duty = duty - 100;
+        if (duty > 100)
+          continue;
+      }
+      if (freq < 1000)
+        duty = 0;
+      if (abs(lastDuty - duty) > 1)
+      {
+        RenderPwmUart(duty, freq);
+        lastDuty = duty;
+      }
+    } else if (signalAcquired && now - lastPulse > 1000)
+    {
+      signalAcquired = false;
+      RenderPwmUart(0, 0);
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -135,22 +153,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -167,9 +185,9 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -182,10 +200,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
 //static void MX_TIM2_Init(void)
 //{
 //
@@ -229,35 +247,11 @@ void SystemClock_Config(void)
 //
 //}
 
-void MX_TIM2_Init(void)
-{
-    TIM_IC_InitTypeDef sConfigIC = {0};
-
-    htim2.Instance = TIM2;
-    htim2.Init.Prescaler = 0;
-    htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim2.Init.Period = 0xFFFFFFFF;
-    htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    HAL_TIM_IC_Init(&htim2);
-
-    // CH1 - Rising edge (period measurement)
-    sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
-    sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
-    sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-    sConfigIC.ICFilter = 0;
-    HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1);
-
-    // CH2 - Falling edge (high time measurement)
-    sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
-    sConfigIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
-    HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2);
-}
-
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -287,13 +281,13 @@ static void MX_USART2_UART_Init(void)
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitTypeDef GPIO_InitStruct = { 0 };
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
@@ -332,33 +326,151 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+void UartClearScreen()
 {
-    if (htim->Instance == TIM2) {
-        if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
-            // Rising edge
-            last_rising = rising;
-            rising = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-        } else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2) {
-            // Falling edge
-            falling = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2);
-            new_data_available = 1;
-        }
-    }
+  printf("\033[2J");
+  printf("\033[H"); // Move cursor to top-left corner
 }
 
-int __io_putchar(int ch) {
-  if (HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY) != HAL_OK) {
+void UartSetCursorPosition(int row, int col)
+{
+  printf("\033[%d;%dH", row, col);
+}
+
+void SetCharAt(int row, int col, char *c)
+{
+  UartSetCursorPosition(row, col);
+  printf(c);
+}
+
+void DrawRisingColumn(int col, int height)
+{
+  for (int r = 1; r < height; r++)
+  {
+    SetCharAt(r, col, "│");
+  }
+  SetCharAt(0, col, "┌");
+  SetCharAt(height, col, "┘");
+}
+
+void DrawFallingColumn(int col, int height)
+{
+  for (int r = 1; r < height; r++)
+  {
+    SetCharAt(r, col, "│");
+  }
+  SetCharAt(0, col, "┐");
+  SetCharAt(height, col, "└");
+}
+
+void DrawRow(int startCol, int endCol, int row)
+{
+  for (int c = startCol; c < endCol; c++)
+  {
+    SetCharAt(row, c, "─");
+  }
+}
+
+static void RenderPwmUart(uint16_t duty, uint32_t freq)
+{
+  char msg[100];
+  uint16_t width = 50;
+  uint16_t height = 8;
+
+  UartClearScreen();
+
+  if (freq < 1000)
+  {
+    snprintf(msg, sizeof(msg), "No signal");
+  } else
+  {
+    snprintf(msg, sizeof(msg), "Freq: %lu Hz, Duty: %d pct\r\n", freq, duty);
+  }
+  UartSetCursorPosition(10, 0);
+  printf(msg);
+
+  if (duty == 0)
+  {
+    DrawRow(0, 2 * width, height);
+    return;
+  }
+
+  // first wave
+  uint16_t secondCol = duty * width / 100;
+  DrawRow(0, secondCol, 0);
+  DrawRow(secondCol, width, height);
+  DrawRisingColumn(0, height);
+  DrawFallingColumn(secondCol, height);
+
+  // second wave
+  uint16_t thirdCol = secondCol + width;
+  DrawRow(width, thirdCol, 0);
+  DrawRow(thirdCol, width * 2, height);
+  DrawRisingColumn(width, height);
+  DrawFallingColumn(thirdCol, height);
+
+  // start of third wave
+  DrawRisingColumn(width * 2, height);
+
+}
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim->Instance == TIM2)
+  {
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+    {
+      // Rising edge
+      last_rising = rising;
+      rising = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+    } else if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
+    {
+      // Falling edge
+      falling = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2);
+      new_data_available = 1;
+    }
+  }
+}
+
+int __io_putchar(int ch)
+{
+  if (HAL_UART_Transmit(&huart2, (uint8_t*) &ch, 1, HAL_MAX_DELAY) != HAL_OK)
+  {
     return -1;
   }
   return ch;
 }
+
+// I don't know how to set this up in .IOC editor, so I hardcode it here. We cannot regenerate code now :-/
+void MX_TIM2_Init(void)
+{
+  TIM_IC_InitTypeDef sConfigIC = { 0 };
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 0xFFFFFFFF;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  HAL_TIM_IC_Init(&htim2);
+
+  // CH1 - Rising edge (period measurement)
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1);
+
+  // CH2 - Falling edge (high time measurement)
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_FALLING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_INDIRECTTI;
+  HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2);
+}
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
